2f7211cffa90f6b5357e957d194a8faaacc2d3ed
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-31 15:13:45 +0400

    feat: created initial backend server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,26 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    println!("Hello, world!");
+    let app: Router = Router::new().route("/", get(root)).layer(
+        CorsLayer::new()
+            .allow_origin(Any)
+            .allow_methods(Any)
+            .allow_headers(Any),
+    );
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("Backend server listening on {}", addr);
+
+    let listener: TcpListener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Data Comparer Backend API"
 }

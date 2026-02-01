c01d273c93cfa05a01cc837109563067bb8e437c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-01 13:46:51 +0400

    feat(fe-FileUpload): added input element for file upload

--- frontend/src/components/file_upload.rs
+++ frontend/src/components/file_upload.rs
@@ -1,11 +1,19 @@
 use data_comparer_shared::Dataset;
-use leptos::prelude::*;
+use leptos::{ev::Event, logging::log, prelude::*};
 
 #[component]
 pub fn FileUpload(on_dataset_loaded: Callback<Dataset>, dataset_name: String) -> impl IntoView {
+    let on_file_change = move |ev: Event| {
+        log!("file changed");
+    };
     view! {
         <div class="file-upload">
             <h3>{dataset_name}</h3>
+            <input
+                type="file"
+                accept=".csv,.xlsx,.xlsm"
+                on:change=on_file_change
+            />
         </div>
     }
 }

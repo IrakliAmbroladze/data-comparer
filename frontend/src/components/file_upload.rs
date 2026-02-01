f143fdfba0370d3ca73b5128c5b08472a43deba1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-01 14:35:35 +0400

    feat(fe-FileUpload): added error Signal

--- frontend/src/components/file_upload.rs
+++ frontend/src/components/file_upload.rs
@@ -4,6 +4,7 @@ use leptos::{ev::Event, logging::log, prelude::*};
 #[component]
 pub fn FileUpload(on_dataset_loaded: Callback<Dataset>, dataset_name: String) -> impl IntoView {
     let (loading, set_loading) = signal(false);
+    let (error, set_error) = signal(None::<String>);
 
     let on_file_change = move |ev: Event| {
         log!("file changed");
@@ -19,6 +20,10 @@ pub fn FileUpload(on_dataset_loaded: Callback<Dataset>, dataset_name: String) ->
             <button>
                 {move || if loading.get() {"Uploading..."} else { "Upload" }}
             </button>
+
+            {move || error.get().map(|e| view! {
+                <p class="error">{e}</p>
+            })}
         </div>
     }
 }

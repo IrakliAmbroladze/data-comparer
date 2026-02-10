51ed37a810f42e1de09f4a7cf699e57fca4b9550
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-10 14:17:23 +0400

    style(grid): added editable-grid css

--- frontend/src/components/editable_grid.rs
+++ frontend/src/components/editable_grid.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn EditableGrid() -> impl IntoView {
     view! {
-        <div>
+        <div class="editable-grid">
             <h3>{"This is an Editable Grid"}</h3>
         </div>
     }

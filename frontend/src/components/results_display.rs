b229df805122fdb6bd7f7a8d1a9bb23dbaa0d355
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-14 20:46:22 +0400

    feat(ResultsDisplay): added initial toggle_sort function for starting sorting

--- frontend/src/components/results_display.rs
+++ frontend/src/components/results_display.rs
@@ -1,10 +1,16 @@
 use data_comparer_shared::ComparisonResult;
-use leptos::prelude::*;
+use leptos::{logging::log, prelude::*};
 
 #[component]
 pub fn ResultsDisplay(result: ComparisonResult) -> impl IntoView {
     let matched = result.matched.clone();
     let matched_count = matched.len();
+
+    let toggle_sort = move |column: &'static str| {
+        move |_| {
+            log!("clicked on {}", column);
+        }
+    };
     view! {
         <div class="results">
             <h2>"Comparison Results"</h2>
@@ -14,12 +20,22 @@ pub fn ResultsDisplay(result: ComparisonResult) -> impl IntoView {
                 <table>
                     <thead>
                         <tr>
-                            <th class="sortable">"ID"</th>
-                            <th class="sortable">"Name (Dataset 1)"</th>
-                            <th class="sortable">"Amount (Dataset 1)"</th>
+                            <th on:click=toggle_sort("id") class="sortable">
+                                "ID"
+                            </th>
+                            <th on:click=toggle_sort("name") class="sortable">
+                                "Name (Dataset 1)"
+                            </th>
+                            <th on:click=toggle_sort("amount1") class="sortable">
+                                "Amount (Dataset 1)"
+                            </th>
                             <th>"Name (Dataset 2)"</th>
-                            <th class="sortable">"Amount (Dataset 2)"</th>
-                            <th class="sortable">"Difference"</th>
+                            <th on:click=toggle_sort("amount2") class="sortable">
+                                "Amount (Dataset 2)"
+                            </th>
+                            <th on:click=toggle_sort("diff") class="sortable">
+                                "Difference"
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
